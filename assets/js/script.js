fadd180950a2f5a2de803802b380f31d49413749
dgstjohn <dgstjohn@gmail.com>
2021-06-27 19:00:51 -0400

    dynamic rows finished except for time display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,98 @@
 // display date in header
+// variable to grab Moment "right now" value, reduced to day + date format
 var currentDay = moment().format("dddd, MMMM D, YYYY");
-//console.log (currentDay);
-var displayDay = $( "#currentDay" );
-//console.log(displayDay);
+// variable to select #currentDay id in header
+var displayDay = $("#currentDay");
+// JQuery text method to display date
 $(displayDay.text(currentDay));
 
-// create rows from 9 am to 5 pm 
+// set variable for container to add dynamically created rows
 var container = $(".container");
-var hours = ["9 am", "10 am", "11 am", "Noon", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
-var militaryHours = [9,10,11,12,13,14,15,16,17];
-var militaryHours2 = [20,21,22,23,0,1,2,3,4];
 
-// loop through hours array to create object-literal container with time designation to left
-for (let index = 0; index < hours.length; index++) {
-    container.append(` 
-    <div class="row">
-            <div class="col-sm-1 hour">${hours[index]}</div>
-            <div class="col-sm-10"><textarea class="form-control h-100" id="textarea"></textarea></div>
-            <div class="col-sm-1"><button class="saveBtn h-100 w-100" id="saveBtn"><i class="far fa-save"></i></button></div>
-        </div> 
-    `);
+// for loop to create dynamic rows 
+for (let i = 9; i < 18; i++) {
+    // create div for whole row with class of "row" (from stylesheet) and id pegged to index
+    // could not find a JQuery equivalent for document.createElement -- add() didn't seem to do it
+    var rowDiv = document.createElement("div");
+    $(rowDiv).attr("class", "row");
+    $(rowDiv).attr("id", i);
+    // create divs to go inside row with Bootstrap and other classes
+    // div for time display column at left
+    var timeColumn = document.createElement("div");
+    $(timeColumn).attr("class","col-sm-1 hour");
+    // div for text input area for tasks
+    var taskColumn = document.createElement("div");
+    $(taskColumn).attr("class","col-sm-10");
+    // add textarea element inside taskColumn
+    var taskColTextarea = document.createElement("textarea");
+    $(taskColTextarea).attr("class","form-control h-100");
+    $(taskColTextarea).attr("id","textarea");
+    $(taskColumn).append(taskColTextarea);
+    // div for save button column at right
+    var saveColumn = document.createElement("div");
+    $(saveColumn).attr("class","col-sm-1");
+    // add button inside saveColumn
+    var saveBtn = document.createElement("button");
+    $(saveBtn).attr("class","saveBtn h-100 w-100");
+    $(saveBtn).attr("id","saveBtn");
+    $(saveColumn).append(saveBtn);
+    // stick Font Awesome save icon inside button
+    var saveIcon = document.createElement("i");
+    $(saveIcon).attr("class","far fa-save")
+    $(saveBtn).append(saveIcon)
+    // append row to container
+    $(container).append(rowDiv);
+    // append divs to row
+    $(rowDiv).append(timeColumn);
+    $(rowDiv).append(taskColumn);
+    $(rowDiv).append(saveColumn);
+}
+
+// var hours = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
+
+// // loop through hours array to create object-literal container with time designation to left
+// for (let index = 0; index < hours.length; index++) {
+//     container.append(` 
+//     <div class="row">
+//             <div class="col-sm-1 hour">${hours[index]}</div>
+//             <div class="col-sm-10"><textarea class="form-control h-100" id="textarea"></textarea></div>
+//             <div class="col-sm-1"><button class="saveBtn h-100 w-100" id="saveBtn"><i class="far fa-save"></i></button></div>
+//         </div> 
+//     `);
 
     // OR
     // make a div with a class that concetenates row + [i]
     // then insert object literal with the three columns as notated above
-}
+// }
 
-var timeNow = moment().format("H");
-console.log(timeNow);
+var militaryHours = ["9", 10, 11, 12, 13, 14, "15", "16", "17"];
+var timeNow = moment();
+// console.log(timeNow);
 for (index = 0; index < militaryHours.length; index++) {
-    if (timeNow > parseInt(militaryHours))  {
-        $( "textarea" ).addClass("past");
+    if (timeNow > parseInt(militaryHours)) {
+        $("textarea").addClass("past");
     }
     else if (timeNow === parseInt(militaryHours)) {
-        $( "textarea" ).addClass("present");
-    } 
+        $("textarea").addClass("present");
+    }
     else {
-        $( "textarea" ).addClass("future");
+        $("textarea").addClass("future");
     }
 }
 
 // get text from input in textarea
-$(".container").on("click", "textarea", function() {
+$(".container").on("click", "textarea", function () {
     console.log(this);
     var task = $(this)
-    .text()
-    .trim();
-  });
+        .text()
+        .trim();
+});
 
-$(".saveBtn").on("click", "button", function() {
-    localStorage.setItem("#saveBtn",task);
+$(".container").on("blur", "textarea", function () {
+
+});
+
+$(".saveBtn").on("click", "button", function () {
+    localStorage.setItem("#saveBtn", task);
     console.log(task);
 })
-
-var startTime = moment(9,"H");
-var timeCounter = 9;
-var taskTime = moment().format();
-for (let index = 0; index < startTime.length; index++) {
-    while (timeCounter < 18)  {
-        return moment(9++,"H")
-        timeCounter++;
-
-    }  
-}
